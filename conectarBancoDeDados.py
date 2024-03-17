d49772a50e2a308fee2993814c29a194ed23a0e2
Fabio Henrique <fabinho_mazetti@hotmail.com>
2024-03-17 12:05:16 -0300

    conectando ao Atlas MongoDB

--- conectarBancoDeDados.py
+++ conectarBancoDeDados.py
@@ -1,3 +1,33 @@
-# fazendo comentario para testar um commit
 
-# fazendo outro comentário para testar novamente.
+from pymongo.mongo_client import MongoClient
+from pprint import pprint
+connectURL = 'mongodb+srv://fbmazetti:12345@cluster-db.blzhilc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-DB' #URL de conexão
+
+client = MongoClient(connectURL)
+
+meubanco = client['db_teste']
+colecao = meubanco['collection-teste']
+document0 = {"type":"trabalho de sistemas de tempo real", "status": "feito"} #documento qualquer
+document1 = {"type":"outro trabalho", "status": "em progresso"} #documento qualquer
+
+#insert_document = colecao.insert_one(document1) #comando para inserir um documento
+#print(f"Documento inserido com sucesso e seu ID é: {insert_document.inserted_id}")
+
+buscar_primeiro_item = colecao.find_one()
+print(buscar_primeiro_item)
+
+buscar_um_item_com_parametro = colecao.find_one({'status': 'em progresso'}) # passando parametros de busca
+print(buscar_um_item_com_parametro)
+
+#coleção exemplo do atlas mongoDB
+# mydatabase = client['sample_mflix']
+# collection = mydatabase['users']
+
+# for doc in collection.find():
+#     pprint(doc)
+   
+client.close()
+
+
+
+
