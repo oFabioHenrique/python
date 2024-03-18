7670ce9c9a8a639f7d77da37c978c6ac15cb9878
oFabioHenrique <fabinho_mazetti@hotmail.com>
2024-03-18 08:32:31 -0300

    atualizando a aula12

--- aula12.py
+++ aula12.py
@@ -1,9 +1,8 @@
-nome = 'Fabio Henrique'
-
-altura = 1.68
-peso = 71
+nome = input(f'insira seu nome: ')
+nome_title = nome.title() # deixa a primeira letra da cada palvra em maiúsculo
+altura = float(input(f'insira sua altura em metros: ')) 
+peso = float(input('insira seu peso em Kgs: '))
 
 imc = peso / (altura * altura)
 
-print(f'Meu nome é {nome}, tenho altura de {altura}M, meu peso é {peso}Kg e meu IMC é: {imc}')
-#teste
+print(f'Meu nome é {nome_title}, tenho altura de {altura}M, meu peso é {peso}Kg e meu IMC é: {imc}')
