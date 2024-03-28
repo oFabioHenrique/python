799cc6d803d02eb3ff2bf70d062f45051ccbf0ba
Fabio Henrique <fabinho_mazetti@hotmail.com>
2024-03-27 23:35:29 -0300

    correções nos códigos

--- aulas_iniciante/aula32.py
+++ aulas_iniciante/aula32.py
@@ -3,22 +3,25 @@ Faça um programa que peça ao usuário para digitar um número inteiro,
 informe se este número é par ou impar. Caso o usuário não digite um número inteiro,
 informe que não é um número inteiro.
 '''
-valor_int_input = input('Digite um número inteiro, por favor \n')
-
-valor_int_int = int(valor_int_input)
-
+#inicio função
 def par_ou_impar(x, y = 2):
     num_par = x % y
     if num_par == 0:
         print(f'Seu número {valor_int_int} é par, pois o resto da divisão por 2 é {num_par}\n')
     else:
-        print(f'Seu número {valor_int_int} é impar, pois o resto da divisão por 2 não é 0\n')
+            print(f'Seu número {valor_int_int} é impar, pois o resto da divisão por 2 não é 0\n')
+#fim função
+            
+valor_int_input = input('Digite um número inteiro, por favor \n')
 
-par_ou_impar(valor_int_int)
-
+if valor_int_input.isdigit():
+    valor_int_int = int(valor_int_input)
+    par_ou_impar(valor_int_int)
+else:
+     print('Você não digitou um número inteiro!')
 '''
 Faça um programa que pegunte a hora ao usuário e, baseando-se no horário descrito, 
-exiba a saudação apropriada. Ex: Bom dia 0-11, Boa tarde 12-17, Boa noite 18-23.
+exiba a saudação apropriada. Ex: Boa Madrugada 1-4, Bom dia 5-11, Boa tarde 12-18, Boa noite 19-00.
 '''
 hora_user = input('Digite o seu horário de agora, por favor \n')
 hora_user_int = int(hora_user)
@@ -46,14 +49,14 @@ Faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letra
 Se tiver entre 5 e 6 letras, escreva "Seu nome é normal", maior que 6 letras escreva "Seu nome é muito grande".
 '''   
 
-primeiro_nome = input('Escreva somente seu primeiro nome, por favor \n')
+# primeiro_nome = input('Escreva somente seu primeiro nome, por favor \n')
 
-qnt_letras = len(primeiro_nome)  #- nome.count(" ")
+# qnt_letras = len(primeiro_nome)  #- nome.count(" ")
 
-if qnt_letras > 6:
-    print(f"Seu nome é muito grande, pois tem {qnt_letras} letras")
-elif qnt_letras >= 5 and qnt_letras <= 6:
-    print(f"Seu nome é normal, pois tem {qnt_letras} letras")
-else:
-    print(f"Seu nome é curto, pois tem {qnt_letras} letras")
+# if qnt_letras > 6:
+#     print(f"Seu nome é muito grande, pois tem {qnt_letras} letras")
+# elif qnt_letras >= 5 and qnt_letras <= 6:
+#     print(f"Seu nome é normal, pois tem {qnt_letras} letras")
+# else:
+#     print(f"Seu nome é curto, pois tem {qnt_letras} letras")
 
